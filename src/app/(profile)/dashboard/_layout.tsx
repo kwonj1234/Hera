09812c44c614e6444543b13c8ace00a0a2ae7059
kwonj1234 <kwonj1234@gmail.com>
2024-03-12 23:56:20 -0500

    start on profile page

---
+++ src/app/(profile)/dashboard/_layout.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+
+import { Button } from "react-native-paper"
+
+export default function ProfileScreen() {
+  return (
+		<View>
+			<Text>Hello USER</Text>
+			<Button
+			>
+				Upload
+			</Button>
+		</View>
+	)
+}
