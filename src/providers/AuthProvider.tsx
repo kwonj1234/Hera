e261ad99f5d921c2ad14614f83ff26598b146d43
kwonj1234 <kwonj1234@gmail.com>
2024-03-15 17:39:28 -0500

    remove console logs

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -20,7 +20,6 @@ export default function AuthProvider({ children } :PropsWithChildren) {
   useEffect(() => {
     const fetchSession = async () => {
       const { data, error } = await supabase.auth.getSession()
-      console.log(data)
       setSession(data.session)
       setLoading(false)
     }
